b7cbc41d242c7587e9f18f5b974b4c807083620b
Adam O'Mahony <adamomahony@gmail.com>
2018-12-15 13:55:41 +0000

    removed wtforms as was casuing error

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ import os
 from datetime import datetime
 from flask import (Flask, render_template, make_response, 
 request, redirect, session, g)
-from wtforms import Form
 import random
 from random import shuffle
 import utills
